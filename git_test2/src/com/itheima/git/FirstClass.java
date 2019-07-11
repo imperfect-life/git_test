0abec5847bebf0fdf0316c88fe083a0a5da2b698
javaclass001 <619620295@qq.com>
2019-07-11 10:54:51 +0800

    修改FirstClass

--- git_test2/src/com/itheima/git/FirstClass.java
+++ git_test2/src/com/itheima/git/FirstClass.java
@@ -9,9 +9,11 @@ public class FirstClass {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        //hello test
     }
     public void test1(){
         int i = 0;
         i+=10;
+        System.out.println("i = " + i);
     }
 }
